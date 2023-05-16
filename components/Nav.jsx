3dcbb5c48a05c8a0dd7be0a9ec4008f37606b1f6
amir4976 <amirhosianahmadi4976@gmail.com>
2023-05-16 19:10:26 +0330

    promptopia

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,38 +2,38 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import {useState,useEffect } from 'react'
-import {singIn,singOut,useSession,getProviders} from 'next-auth/react'
+import { useState, useEffect } from 'react'
+import { singIn, singOut, useSession, getProviders } from 'next-auth/react'
 
 
 
 
 function Nav() {
-  const isUserLoggedIn = false
-  const [provider ,setProvider] = useState(null)
+  const isUserLoggedIn = true
+  const [provider, setProvider] = useState(null)
   const [ToggleDropDown, setToggleDropDown] = useState(false)
 
 
-  useEffect(()=>{
-    const setProvider = async () =>{
+  useEffect(() => {
+    const setProvider = async () => {
       const response = await getProviders()
       setProvider(response)
     }
     setProvider()
-  },[])
+  }, [])
 
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href='/' className='flex gap-2 flex-center'>
-        <Image 
-        src='./images/logo.svg'
-        alt='promptoipa logo'
-        width={30}
-        height={30}
-      className='object-contain'
-/>      
-      <p className='logo_text'>PROMPTOPIA</p>
+        <Image
+          src='./images/logo.svg'
+          alt='promptoipa logo'
+          width={30}
+          height={30}
+          className='object-contain'
+        />
+        <p className='logo_text'>PROMPTOPIA</p>
       </Link>
 
 
@@ -41,33 +41,59 @@ function Nav() {
         {isUserLoggedIn ? (
           <div className='flex gap-3 md:gap-5'>
             <Link href={'/create-prompt'} className='black_btn'>create post </Link>
-            <button type='button' onClick={singOut} className ='outline_btn '>
+            <button type='button' onClick={singOut} className='outline_btn '>
               sign Out
             </button>
-             <Link href={'/profile'}>
+            <Link href={'/profile'}>
 
-              <Image src='./images/logo.svg'  width={37} height={37} className='rounded-full'/>
+              <Image src='./images/logo.svg' width={37} height={37} className='rounded-full' />
             </Link>
           </div>
-        ):(
+        ) : (
           <>
-            {provider && Object.values(provider).map((provider)=>(
-                <button type='button'
+            {provider && Object.values(provider).map((provider) => (
+              <button type='button'
                 key={provider.name}
-                onClick={()=> singIn(provider.id) }
+                onClick={() => singIn(provider.id)}
                 className='black_btn'
-                >
-                  sign Up
-                </button>
+              >
+                sign Up
+              </button>
             ))}
           </>
         )}
 
 
       </div>
-            {/* MOBILE NAVIGATION */}
+      {/* MOBILE NAVIGATION */}
 
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image src='./images/logo.svg' 
+            width={37} 
+            height={37} 
+            className='rounded-full'
+            alt='profile' 
+            onClick={}
+            />
 
+          </div>
+        ):(
+          <>
+          {provider && Object.values(provider).map((provider) => (
+            <button type='button'
+              key={provider.name}
+              onClick={() => singIn(provider.id)}
+              className='black_btn'
+            >
+              sign Up
+            </button>
+          ))}
+        </>
+        
+        )}
+      </div>
 
     </nav>
   )
